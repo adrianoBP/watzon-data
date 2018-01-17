2b2c8cebabaab74c1ff7d88b1b133aac180719d6
amreo <amreo@live.it>
2018-01-17 14:26:52 +0100

    Added initial project files

---
+++ src/test/java/it/sorintlab/watzondata/WatzonDataApplicationTests.java
@@ -0,0 +1,16 @@
+package it.sorintlab.watzondata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WatzonDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
