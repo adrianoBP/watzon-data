3bea8c00b3bb72695e68bf735307b1bd19d9f55c
Adriano Boccardo Pochettino <adriano.boccardo@gmail.com>
2018-01-18 14:17:29 +0100

    Added products repo + package

--- src/main/java/it/sorintlab/watzondata/repository/CustomerRepository.java
+++ src/main/java/it/sorintlab/watzondata/repository/CustomerRepository.java
@@ -1,4 +1,4 @@
-package it.sorintlab.watzondata.controller;
+package it.sorintlab.watzondata.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
