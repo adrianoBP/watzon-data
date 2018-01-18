5e887270c7c243ac5217fc64a03179262e36f2b9
Adriano Boccardo Pochettino <adriano.boccardo@gmail.com>
2018-01-18 15:02:12 +0100

    Added search methods

--- src/main/java/it/sorintlab/watzondata/domain/CustomerProductPk.java
+++ src/main/java/it/sorintlab/watzondata/domain/CustomerProductPk.java
@@ -41,42 +41,35 @@ public class CustomerProductPk implements Serializable {
 		return serialVersionUID;
 	}
 
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = 1;
-//		result = prime * result + ((customer == null) ? 0 : customer.hashCode());
-//		result = prime * result + ((product == null) ? 0 : product.hashCode());
-//		return result;
-//	}
-//
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() == obj.getClass())
-//		{
-//			CustomerProductPk other = (CustomerProductPk) obj;
-//		if (customer == null) {
-//			if (other.customer != null)
-//				return false;
-//		} else if (!customer.equals(other.customer))
-//			return false;
-//		if (product == null) {
-//			if (other.product != null)
-//				return false;
-//		} else if (!product.equals(other.product))
-//			return false;
-//		return true;
-//		}
-//		else if(obj.getClass() == String.class) {
-//			int temp = Integer.parseInt((obj).toString());
-//			return temp==product.getId();
-//			
-//		}else
-//			return false;
-//		}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CustomerProductPk other = (CustomerProductPk) obj;
+		if (customer == null) {
+			if (other.customer != null)
+				return false;
+		} else if (!customer.equals(other.customer))
+			return false;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		return true;
+	}
 
 }
--- src/main/java/it/sorintlab/watzondata/repository/CustomerRepository.java
+++ src/main/java/it/sorintlab/watzondata/repository/CustomerRepository.java
@@ -1,11 +1,20 @@
 package it.sorintlab.watzondata.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import it.sorintlab.watzondata.domain.Customer;
 
 @RepositoryRestResource
 public interface CustomerRepository extends PagingAndSortingRepository<Customer, Integer>{
+
+	@RestResource(path="search", rel="search")
+	public Page<Customer> findByName(@Param("name") String name, Pageable pagin);
+	@RestResource(path="searchlike", rel="searchlike")
+	public Page<Customer> findByNameContaining(@Param("name") String name, Pageable pagin);
 
 }
