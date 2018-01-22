c6b4a04219b18723236b54961bff0031e6455324
Adriano Boccardo Pochettino <adriano.boccardo@gmail.com>
2018-01-22 09:22:57 +0100

    ProductReposotory fixing

--- src/main/java/it/sorintlab/watzondata/repository/ProductsRepository.java
+++ src/main/java/it/sorintlab/watzondata/repository/ProductsRepository.java
@@ -2,14 +2,11 @@ package it.sorintlab.watzondata.repository;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-import it.sorintlab.watzondata.backend.Customer;
 import it.sorintlab.watzondata.backend.Product;
 
 @RepositoryRestResource
